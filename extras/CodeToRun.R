c0800191cabdba1998bc55224b53d5724c87b328
ted <ted9219@gmail.com>
2019-05-14 22:08:26 +0900

    fix

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -1,20 +1,18 @@
 library(mortality)
-library(FeatureExtraction)
-library(PatientLevelPrediction)
 # USER INPUTS
 #=======================
 # The folder where the study intermediate and result files will be written:
-outputFolder <- "c:/Users/User/Desktop/Progress/Mortality_Result"
+outputFolder <- "./mortalityResults"
 
 # Specify where the temporary files (used by the ff package) will be created:
-options(fftempdir = "c:/Users/User/Desktop/Progress/Mortality_temp")
+options(fftempdir = "location with space to save big data")
 
 # Details for connecting to the server:
-dbms <- "sql server"
-user <- 'ted9219'
-pw <- 'rddo678$'
-server <- '128.1.99.53'
-port <- '1433'
+dbms <- "you dbms"
+user <- 'your username'
+pw <- 'your password'
+server <- 'your server'
+port <- 'your port'
 
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 server = server,
@@ -23,14 +21,14 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 port = port)
 
 # Add the database containing the OMOP CDM data
-cdmDatabaseSchema <- 'NHIS_NSC.dbo'
+cdmDatabaseSchema <- 'cdm database schema'
 # Add a database with read/write access as this is where the cohorts will be generated
-cohortDatabaseSchema <- 'NHIS_NSC_Result.dbo'
+cohortDatabaseSchema <- 'work database schema'
 
 oracleTempSchema <- NULL
 
 # table name where the cohorts will be generated
-cohortTable <- 'CS_mortality'
+cohortTable <- 'mortalityCohort'
 #=======================
 
 execute(connectionDetails = connectionDetails,
@@ -42,7 +40,6 @@ execute(connectionDetails = connectionDetails,
         createCohorts = F,
         runAnalyses = F,
         createResultsDoc = F,
-        packageResults = T,
+        packageResults = F,
         createValidationPackage = F,
-        minCellCount= 5,
-        cdmVersion = 5)
+        minCellCount= 5)
