dbff2b8dd7b9afda684a9b9f670f2fa4ac24d3c9
N.K <clax1412@aumc.ac.kr>
2019-05-15 15:57:47 +0900

    First commit...

---
+++ extras/CodeToRun.R
@@ -0,0 +1,48 @@
+library(mortality)
+library(FeatureExtraction)
+library(PatientLevelPrediction)
+# USER INPUTS
+#=======================
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "c:/Users/User/Desktop/Progress/Mortality_Result"
+
+# Specify where the temporary files (used by the ff package) will be created:
+options(fftempdir = "c:/Users/User/Desktop/Progress/Mortality_temp")
+
+# Details for connecting to the server:
+dbms <- Sys.getenv('dbms')
+user <- Sys.getenv('user')
+pw <- Sys.getenv('password')
+server <- Sys.getenv('server')
+port <- Sys.getenv('port')
+
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
+                                                                server = server,
+                                                                user = user,
+                                                                password = pw,
+                                                                port = port)
+
+# Add the database containing the OMOP CDM data
+cdmDatabaseSchema <- 'NHIS_NSC.dbo'
+# Add a database with read/write access as this is where the cohorts will be generated
+cohortDatabaseSchema <- 'NHIS_NSC_Result.dbo'
+
+oracleTempSchema <- NULL
+
+# table name where the cohorts will be generated
+cohortTable <- 'CS_mortality'
+#=======================
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        outputFolder = outputFolder,
+        createProtocol = F,
+        createCohorts = F,
+        runAnalyses = F,
+        createResultsDoc = F,
+        packageResults = T,
+        createValidationPackage = F,
+        minCellCount= 5,
+        cdmVersion = 5)
